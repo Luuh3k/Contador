6345fc48225f6b0080466bb2cca710ccd581ae0c
Luuh3k <lucas.digital18@gmail.com>
2022-06-14 23:02:13 -0300

    revendo console

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -4,10 +4,12 @@ var currentNumber = 0;
 function increment() {
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    console.log(currentNumber)
 }
 
 function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    console.log(currentNumber)
 }
 
